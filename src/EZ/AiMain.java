4cc24dc373b185e4c43a23b2c6ea32d756cc7741
zhuyingshan <313437903@qq.com>
2016-05-03 09:51:33 +0800

    矛的代码和在main中的代码

--- src/EZ/AiMain.java
+++ src/EZ/AiMain.java
@@ -1,29 +1,31 @@
 package EZ;
 
-import Spear.Spear;
 
 public class AiMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		InformationReceive IR=new InformationReceive();		
-		GameIniInformation GII=new GameIniInformation(IR.inReceive());
+		InformationReceive IR=new InformationReceive();
+		IR.inReceive();		
+		GameIniInformation GII=new GameIniInformation();
 		if(GII.samuraiID==0||GII.samuraiID==3){
-			
 			for(int i=1;i<GII.totalRounds;i++){
-				TurnInformationReceive TIR=new TurnInformationReceive();				
-				TurnInformation TIF=new TurnInformation(TIR.turnInformation,TIR.battleField);
-				Spear spear=new Spear();
-
-				
+				SpearAi spear=new SpearAi();//spear
+                if(spear.recover!=0){//Spear先判断自己是否死亡，再去判断战场状态
+						System.out.print(0+"\n");
+						break;
+				}else {
+					spear.analyseEnemy();
+					spear.analyseEnemyCourt();
+					}
+				System.out.print(spear.order);//输出指令到控制台，manager自己读取命令	
 			}
+			
 			
 		}else if(GII.samuraiID==1||GII.samuraiID==4){
 			
 			for(int i=1;i<GII.totalRounds;i++){
-				TurnInformationReceive TIR=new TurnInformationReceive();				
-				TurnInformation TIF=new TurnInformation(TIR.turnInformation,TIR.battleField);
 				Sword sword=new Sword();
 //				sword.
 				
@@ -32,8 +34,6 @@ public class AiMain {
 		}else if(GII.samuraiID==2||GII.samuraiID==5){
 			
 			for(int i=1;i<GII.totalRounds;i++){
-				TurnInformationReceive TIR=new TurnInformationReceive();				
-				TurnInformation TIF=new TurnInformation(TIR.turnInformation,TIR.battleField);
 				Battleax battleax=new Battleax();
 //				battleax.
 				
